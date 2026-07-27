8db02ae3414a8b75cb3b99441e1e3483e42ea948
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-07-27 15:34:33 -0400

    entity classes

---
+++ S26Week12BlazorWithDb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace S26Week12BlazorWithDb.Models
+{
+    public class Category
+    {
+        // scalar properties
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        // navigation property
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ S26Week12BlazorWithDb/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace S26Week12BlazorWithDb.Models
+{
+    public class Product
+    {
+        // scalar properties
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public double? Price { get; set; }
+        public int? CategoryId { get; set; }
+
+        // navigation property
+        public Category? Category { get; set; }
+    }
+}
